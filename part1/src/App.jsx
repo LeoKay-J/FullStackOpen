01c6fc7a8c280f1e8b2fb51298f0d85436fb07a0
Leokayjones <leo.kayjones@loafalo.com>
2025-08-08 13:14:00 +0300

    update app.jsx

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,3 +1,31 @@
+const Header = ({courseName}) => {
+    return(
+      <div>
+      <h1>{courseName}</h1>
+      </div>
+  )
+}
+
+const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
+      return(
+    <div>
+      <Part   part={part1} exercises={exercises1}/>
+      <Part   part={part2} exercises={exercises2}/>
+      <Part   part={part3} exercises={exercises3}/>
+  </div>
+  )
+}
+
+const Part = ({part, exercises,}) => {
+    return(
+      <div>
+        <p>
+        {part} {exercises}
+      </p>
+      </div>
+
+  )
+}
 const Total = ({exercises1, exercises2, exercises3}) => {
     return(
       <div>
@@ -6,30 +34,6 @@ const Total = ({exercises1, exercises2, exercises3}) => {
     )
 }
 
-
-const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
-      return(
-    <div>
-        <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-  </div>
-  )
-}
-
-const Header = ({courseName}) => {
-    return(
-      <div>
-      <h1>{courseName}</h1>
-      </div>
-  )
-}
 
 const App = () => {
   const course = 'Half Stack application development'
