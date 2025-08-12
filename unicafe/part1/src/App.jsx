9e1a1cb2a2e0b70fbc12e141af9076efc7c227b2
Leokayjones <leokayjones@gmail.com>
2025-08-12 13:39:19 +0300

    1.8 tallennus aalto sulut valmiiksi

--- unicafe/part1/src/App.jsx
+++ unicafe/part1/src/App.jsx
@@ -6,18 +6,26 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
 
-const Statistics = () => {
+const Statistics = {good, neutral, bad} => {
+ 
+     const Good = good
+     const Neutral = neutral
+     const Bad = bad
+     const All = good + neutral + bad
+     const Average = (good * 1 + neutral * 0 + bad * -1)/(good + neutral + bad)
+     const Positive = good /(good + neutral + bad)
 
-    const good = good
-    const neutral = neutral
-    const bad = bad
-    const all = good + neutral + bad
-    const average = (good * 1 + neutral * 0 + bad * -1 ) / (good + neutral + bad)
-    const positive = good / (good + neutral + bad)*100
-
-
-  
-  
+     
+    return(
+      <div>
+        <p>Good: (Good)</p>
+        <p>neutral: (Neutral)</p>
+        <p>bad: (Bad)</p>
+        <p>all: (All)</p>
+        <p>average: (Average)</p>
+        <p>positive: (Positive) </p>
+      </div>
+    )
 }
 
 
@@ -28,8 +36,10 @@ const Statistics = () => {
       <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
       <button onClick={() => setBad(bad + 1) }>Bad</button>
 
-      <p></p>
-      </div>
+      <Statistic good={good} neutral={neutral} bad={bad} all={good + neutral + bad} />
+    
+
+     </div>
   )
 }
 
