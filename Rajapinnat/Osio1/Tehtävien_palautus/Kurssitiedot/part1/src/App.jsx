9c46970dc40bc1ca8353880c7f3e73911331fcb5
Leokayjones <leokayjones@gmail.com>
2025-08-14 12:04:34 +0300

    2.4 tehtävä tehty

--- Rajapinnat/Osio1/Tehtävien_palautus/Kurssitiedot/part1/src/App.jsx
+++ Rajapinnat/Osio1/Tehtävien_palautus/Kurssitiedot/part1/src/App.jsx
@@ -21,7 +21,7 @@ const Part = ({part}) => {
       </div>
 )
 
-} 
+}
 
 
 const Content = ({parts}) => {
@@ -35,38 +35,62 @@ const Content = ({parts}) => {
 
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11, 
-      }
-    ]
-  }
+  const course = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  
 
   return (
     <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-
+      {course.map(course =>
+        <div key={course.id}>
+          <Header course={course.name}/>
+          <Content parts={course.parts}/>
+          <Total parts={course.parts}/>
+        </div>
+      )}
     </div>
   )
 }
