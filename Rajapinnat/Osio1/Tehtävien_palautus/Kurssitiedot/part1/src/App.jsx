8d19c05a6004fe1c1f6c76a4ce5d52630f54e9f7
Leokayjones <leokayjones@gmail.com>
2025-08-14 13:32:51 +0300

    tehty tehtävä 2.5 :)

--- Rajapinnat/Osio1/Tehtävien_palautus/Kurssitiedot/part1/src/App.jsx
+++ Rajapinnat/Osio1/Tehtävien_palautus/Kurssitiedot/part1/src/App.jsx
@@ -1,41 +1,8 @@
-const Total = ({parts}) => {
-    const total = parts.reduce((sum, part)=> sum + part.exercises, 0)
-    
-    return(
-     <div>
-      <p>Total Exercixes {total}</p>
-     </div>
-     
-    )
-}
-
-const Header = ({course}) => {
-    return(
-      <h1>{course}</h1> 
-  )
-}
-const Part = ({part}) => {
-    return(
-      <div>
-        <p>{part.name} {part.exercises}</p>
-      </div>
-)
-
-}
-
-
-const Content = ({parts}) => {
-  return(
-    <div>
-      {parts.map(part => <Part key={part.name} part={part}/>)}
-    </div>
-
-  )
-}
-
+import Course from './Courses'
 
 const App = () => {
-  const course = [
+  
+const course = [
     {
       name: 'Half Stack application development',
       id: 1,
@@ -84,15 +51,10 @@ const App = () => {
 
   return (
     <div>
-      {course.map(course =>
-        <div key={course.id}>
-          <Header course={course.name}/>
-          <Content parts={course.parts}/>
-          <Total parts={course.parts}/>
-        </div>
-      )}
+      <Course course={course}/>
     </div>
-  )
-}
+)}
+  
+
 
 export default App
