2767528ef6fadeada1197d65b4c4e421c74a3f3e
Leokayjones <leokayjones@gmail.com>
2025-08-15 10:39:00 +0300

    autokauppa tunti tehtävä. kertausta

---
+++ Rajapinnat/Osio1/Tehtävien_palautus/Autokauppa/part1/src/App.jsx
@@ -0,0 +1,30 @@
+const Tiedot = ({auto}) => {
+  
+    return(
+    <div>
+    <h3>{auto.merkki}</h3>
+    <p>{auto.malli}</p>
+    <p>{auto.hinta}</p>
+    <p>{auto.km}</p>
+    </div> )
+  
+
+}
+const App = () => {
+  const autot = [
+    {merkki: "Honda", malli: "Civic", hinta: 2000, km: 150000, id: 1},
+    {merkki: "Fiat", malli: "Punto", hinta: 1000, km: 200000, id: 2},
+    {merkki: "Nissan", malli: "GTR R34", hinta: 170000, km: 50000, id: 3}
+]  
+ 
+
+
+return (
+  <div>
+    <h1>Meidän Autot</h1>
+    {autot.map(auto => <Tiedot key={auto.id} auto={auto}/>)} 
+ </div>
+  )
+}
+
+export default App
