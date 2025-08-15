31820bf03804c3c2d72e5de99ceef79e964f7181
Leokayjones <leokayjones@gmail.com>
2025-08-15 10:46:15 +0300

    Autokauppa. Malli, Merkki, hinta, km kitjoitettu

--- Rajapinnat/Osio1/Tehtävien_palautus/Autokauppa/part1/src/App.jsx
+++ Rajapinnat/Osio1/Tehtävien_palautus/Autokauppa/part1/src/App.jsx
@@ -2,22 +2,36 @@ const Tiedot = ({auto}) => {
   
     return(
     <div>
-    <h3>{auto.merkki}</h3>
-    <p>{auto.malli}</p>
-    <p>{auto.hinta}</p>
-    <p>{auto.km}</p>
+    <h2>Malli: {auto.malli}</h2>
+    <h3>Merkki: {auto.merkki}</h3>
+    <p>Auton hinta: {auto.hinta}€</p>
+    <p>Ajetut kilometrit: {auto.km}</p>
     </div> )
-  
-
 }
 const App = () => {
   const autot = [
-    {merkki: "Honda", malli: "Civic", hinta: 2000, km: 150000, id: 1},
-    {merkki: "Fiat", malli: "Punto", hinta: 1000, km: 200000, id: 2},
-    {merkki: "Nissan", malli: "GTR R34", hinta: 170000, km: 50000, id: 3}
+    {
+      merkki: "Honda",
+       malli: "Civic", 
+       hinta: 2000, 
+       km: 150000, 
+       id: 1
+      },
+    {
+      merkki: "Fiat", 
+      malli: "Punto", 
+      hinta: 1000, 
+      km: 200000, 
+      id: 2
+    },
+    {
+      merkki: "Nissan", 
+      malli: "GTR R34", 
+      hinta: 170000, 
+      km: 50000, 
+      id: 3
+    }
 ]  
- 
-
 
 return (
   <div>
