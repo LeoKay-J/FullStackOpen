af4ff9744aca3a0fef16d48120dc9a97d59c4a8e
Leokayjones <leokayjones@gmail.com>
2025-08-15 13:03:46 +0300

    autokauppa ei vielä toimi

--- Rajapinnat/Osio1/Tehtävien_palautus/Autokauppa/part1/src/App.jsx
+++ Rajapinnat/Osio1/Tehtävien_palautus/Autokauppa/part1/src/App.jsx
@@ -1,43 +1,40 @@
-const Tiedot = ({auto}) => {
-  
+import { useState } from 'react'
+
+const Tiedot = ({auto, kasittelija}) => {
     return(
     <div>
-    <h2>Malli: {auto.malli}</h2>
-    <h3>Merkki: {auto.merkki}</h3>
-    <p>Auton hinta: {auto.hinta}€</p>
-    <p>Ajetut kilometrit: {auto.km}</p>
+      <h2>Malli: {auto.malli}</h2>
+      <h3>Merkki: {auto.merkki}</h3>
+      <p>Auton hinta: {auto.hinta}€</p>
+      <p>Ajetut kilometrit: {auto.km}</p>
+      <button onClick={kasittelija}>Lisää ostoskoriin</button>
     </div> )
 }
 const App = () => {
-  const autot = [
-    {
-      merkki: "Honda",
-       malli: "Civic", 
-       hinta: 2000, 
-       km: 150000, 
-       id: 1
-      },
-    {
-      merkki: "Fiat", 
-      malli: "Punto", 
-      hinta: 1000, 
-      km: 200000, 
-      id: 2
-    },
-    {
-      merkki: "Nissan", 
-      malli: "GTR R34", 
-      hinta: 170000, 
-      km: 50000, 
-      id: 3
-    }
-]  
+  const [ostoskori, setOstoskori] = useState([])
 
-return (
-  <div>
-    <h1>Meidän Autot</h1>
-    {autot.map(auto => <Tiedot key={auto.id} auto={auto}/>)} 
- </div>
+
+
+  const autot = [
+    {merkki: "Honda",malli: "Civic", hinta: 2000, km: 150000, id: 1},
+    {merkki: "Fiat", malli: "Punto", hinta: 1000, km: 200000, id: 2},
+    {merkki: "Nissan", malli: "GTR R34", hinta: 170000, km: 50000, id: 3}
+  ]  
+
+  const lisaaOstoskoriin = (auto) => {
+    let uusiLista = ostoskori.concat(auto)
+    setOstoskori(uusiLista)
+  }
+
+  return (
+    <div>
+      <h1>Meidän Autot</h1>
+        <div>
+          {autot.map(auto => <Tiedot key={auto.id} auto={auto} kasittelija={lisaaOstoskoriin} />)} 
+        </div>
+        <h1>Ostoskori</h1>
+        {ostoskori.map(auto => <p key={auto.id} >{auto.merkki}</p>)}
+  </div>
   )
 }
 
