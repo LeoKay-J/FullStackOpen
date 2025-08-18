7590a1b009765c17a7a0324a54cc5a4396f64103
Leokayjones <leokayjones@gmail.com>
2025-08-18 13:00:42 +0300

    tehtävä 2.6 valmis vihdoin

--- Rajapinnat/Osio1/Tehtävien_palautus/puhelinluettelo/part1/src/App.jsx
+++ Rajapinnat/Osio1/Tehtävien_palautus/puhelinluettelo/part1/src/App.jsx
@@ -1,33 +1,51 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([ { name: 'Arto Hellas' } ]) 
-  const [newName, setNewName] = (event) =>{
+  const [people, setPeople] = useState([ 
+    { name: 'Arto Hellas' } ]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
     event.preventDefault()
+    const peopleObject ={
+      name: newName,
+      content: newName
+    }
+    setPeople(people.concat(peopleObject))
+    setNewName('')
   }
 
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+
+  }
+  const List = ({people}) => {
+    return(
+      <li>{people.name}</li>
+    )
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-        <ul>
-          {persons.map(persons => 
-          <persons key={persons.id} persons={persons}/>)}
-        </ul>
-        <form onSubmit={newName}>
-            <div>
-              name: <input />
-              <button type="submit">add</button>
-            
-            </div>
-            
+        
+        
+        <form onSubmit={addName}>
+          name: <input value={newName}
+          onChange={handleNameChange}
+          />
+          <button type="submit">add</button>
         </form>
 
-      <h2>Numbers</h2>
+        
+        
+        <h2>Numbers</h2>
       
+      <ul>
+          {people.map(people => 
+          <List key={people.id} people={people}/>)}
+        </ul>
     </div>
   )
-
 }
-
 export default App
