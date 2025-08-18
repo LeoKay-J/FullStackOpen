4719b69496fd64586acc9bcbb5d68b0811c636ef
Leokayjones <leokayjones@gmail.com>
2025-08-18 11:49:30 +0300

    Aloitin tekemään 2.6 tehvävää

--- Rajapinnat/Osio1/Tehtävien_palautus/puhelinluettelo/part1/src/App.jsx
+++ Rajapinnat/Osio1/Tehtävien_palautus/puhelinluettelo/part1/src/App.jsx
@@ -1,27 +1,30 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
-  
-
+  const [persons, setPersons] = useState([ { name: 'Arto Hellas' } ]) 
+  const [newName, setNewName] = (event) =>{
+    event.preventDefault()
   }
+
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>  
-      </form>
-      <h2>Numbers</h2>
+        <ul>
+          {persons.map(persons => 
+          <persons key={persons.id} persons={persons}/>)}
+        </ul>
+        <form onSubmit={newName}>
+            <div>
+              name: <input />
+              <button type="submit">add</button>
+            
+            </div>
+            
+        </form>
 
+      <h2>Numbers</h2>
+      
     </div>
   )
 
