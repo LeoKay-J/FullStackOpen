3e19f2d02265d745989dd11052a079ecaaa553a5
Leokayjones <leokayjones@gmail.com>
2025-08-21 10:19:07 +0300

    2.10 ja 2.11 tehty

--- Rajapinnat/Osio1/Tehtävien_palautus/puhelinluettelo/part1/src/App.jsx
+++ Rajapinnat/Osio1/Tehtävien_palautus/puhelinluettelo/part1/src/App.jsx
@@ -1,71 +1,119 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const App = () => {
-  const [people, setPeople] = useState([ 
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-
-  
-  
-  const addName = (event) => {
-    event.preventDefault()
-
-      const found = people.find(person => person.name === newName)
-      if(found){
-        alert(`${newName} is already added to phonebook`)
-        return
-      }
-      const peopleObject ={
-        name: newName,
-        number: newNumber
-      }
-
-    setPeople(people.concat(peopleObject))
-    setNewName('')
-  }
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-
-  }
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-
-  }
-  const List = ({people}) => {
-    return(
-      <li>{people.name} {people.number}</li>
-    )
-  }
-
-
+const FilterNames = ({ filter, search }) => {
   return (
     <div>
       <h2>Phonebook</h2>
-        
-        <
-        
-        <form onSubmit={addName}> 
-          <div >
-            name: <input value={newName} onChange={handleNameChange}/></div>
-            number:<input value={newNumber} onChange={handleNumberChange}/>
-          <div><button type="submit">add</button></div>
-        </form>
-
-        
-        
-        <h2>Numbers</h2>
-      
-        <ul>
-          {people.map(people => 
-          <List key={people.name} people={people}/>)}
-        </ul>
-        
-        
+      <div>
+        Filter Names: <input value={filter} onChange={search} />
+      </div>
     </div>
   )
 }
+
+const NewNameAndNumber = ({ addName, newName, handleNameChange, newNumber, handleNumberChange }) => {
+  return (
+    <div>
+      <h2>Add New</h2>
+      <form onSubmit={addName}>
+        <div>
+          Name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          Number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+const List = ({ person }) => {
+  return (
+    <li>{person.name} {person.number}</li>
+  )
+}
+
+const NamesAndNumbers = ({ peopleToShow }) => {
+  return (
+    <div>
+      <h2>Numbers</h2>
+      <ul>
+        {peopleToShow.map(person =>
+          <List key={person.id} person={person} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+const App = () => {
+  const [people, setPeople] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  // Fetch people from server
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPeople(response.data)
+      })
+  }, [])
+
+  const addName = (event) => {
+    event.preventDefault()
+    const found = people.find(person => person.name === newName)
+
+    if (found) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
+    const peopleObject = {
+      name: newName,
+      number: newNumber,
+      id: people.length + 1
+    }
+
+    setPeople(people.concat(peopleObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const Search = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const peopleToShow = filter === ''
+    ? people
+    : people.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+
+  return (
+    <div>
+      <FilterNames filter={filter} search={Search} />
+      <NewNameAndNumber
+        addName={addName}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <NamesAndNumbers peopleToShow={peopleToShow} />
+    </div>
+  )
+}
+
 export default App
