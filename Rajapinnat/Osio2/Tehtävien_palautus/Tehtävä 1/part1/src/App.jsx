de4f4341da92844ec719d9469a8321e1cbfd2268
Leokayjones <leokayjones@gmail.com>
2025-08-25 12:01:05 +0300

    tehtävä 1 ei toimi

--- Rajapinnat/Osio2/Tehtävien_palautus/Tehtävä 1/part1/src/App.jsx
+++ Rajapinnat/Osio2/Tehtävien_palautus/Tehtävä 1/part1/src/App.jsx
@@ -1,20 +1,20 @@
-
-
 const App = () => {
 
   <div>
   </div>
 
+  const JokeRandomizer = () => {
+    fetch("https://programming-quotesapi.vercel.app/api/random")
+          .then((response) => response.json())
+          .then((quote) => console.log(quote)); 
+  }
 
   return (
     <div>
-      <button>Random Joke</button>
+      <button onClick={JokeRandomizer}>Random Joke</button>
     </div>
   )
 }
-
-
-
 
 
 export default App
