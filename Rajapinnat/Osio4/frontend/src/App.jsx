20e32f75dcf74c0ff08379a70bb47f7ed1f85b64
Leokayjones <leokayjones@gmail.com>
2025-09-04 13:00:00 +0300

    Ohjelmointisanomat  tehtävä 1.3

--- Rajapinnat/Osio4/frontend/src/App.jsx
+++ Rajapinnat/Osio4/frontend/src/App.jsx
@@ -5,16 +5,27 @@ const App = () => {
 
   const randomCodingQuote = async () => {
     const response = await fetch('http://localhost:3001/api/quotes/random')
-      const data = await response.json()
-      setQuotes(data)
+    const data = await response.json()
+    setQuotes(data)
   }
-  
+
   return (
     <div>
-      <p>Random coding quotes</p>
-      <button onClick={randomCodingQuote}>Press for random coding quote</button>
-      <p>{quotes.quote}</p>
-      <p>{quotes.author}</p>
+      <div>
+        <h3>Random coding quotes</h3>
+        <button onClick={randomCodingQuote}>Press for random coding quote</button>
+        <p>"{quotes.quote}"</p>
+        <p>-{quotes.author}</p>
+      </div>
+      <div>
+        <h3>Add quote</h3>
+        <form>
+          <input placeholder='-Quote-'/>
+          <input placeholder='-Author-'/>
+          <input type="submit" value="Submit"/>
+        </form>
+        
+      </div>
     </div>
   )
 }
