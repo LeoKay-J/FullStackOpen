d640dfc0103a6a34679613a2051ce913f5777f96
Leokayjones <leokayjones@gmail.com>
2025-11-06 13:25:01 +0200

    tietovisa tehtävä backend valmis frontendiä tekeillä

--- Tietokantojen_Hyödyntäminen/Osio3/Tietovisa/frontend/src/App.jsx
+++ Tietokantojen_Hyödyntäminen/Osio3/Tietovisa/frontend/src/App.jsx
@@ -2,33 +2,70 @@ import React, { useState, useEffect } from "react"
 import axios from 'axios'
 import './app.css'
 
-
-
-
 function App() {
 
-const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState([])
+  const [questions, setQuestions] = useState([])
+
+  const [selectedCategory, setSelectedCategories] = useState("")
 
 
-useEffect(()=> {
-  axios.get('http://localhost:3000/categories')
-  .then((response) => {
-    setCategories(response.data)
-  })
-})
+
+
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/categories")
+      .then((response) => {
+        setCategories(response.data);
+      })
+  }, [])
+
+  const quizStart = () => {
+    axios.get('http://localhost:3000/questions/' + selectedCategory)
+      .then((response) => {
+        setQuestions(response.data)
+        console.log(questions)
+      })
+
+    if (selectedCategory) {
+      return (
+        <div className="QuizAnswerBox">
+          <h2>Quiz</h2>
+          <form>
+            <input type="text" placeholder="--answer--" />
+            <button>submit answer</button>
+          </form>
+          {questions.map(question => <li key={question.id}> {questions}</li>)}
+        </div>
+      )
+    }
+    else (selectedCategory = null) => {
+      <div>
+        <p>Please select category</p>
+      </div>
+
+    }
+  }
+
+
+
   return (
-   <div>
-    <div className="HeaderStyle">
-      <h1>TietoVisa</h1>
-    </div>
     <div>
-      <select name="Select">
-        <option>--select category--</option>
-        <option>{categories.map(</option>
-      </select>
+      <div className="HeaderStyle">
+        <h1>Quiz</h1>
+      </div>
+      <div className="CategorySelect">
+        <h2>Select category for quiz</h2>
+        <select onChange={(e) => setSelectedCategories(e.target.value)}>
+          <option>--select category--</option>
+          {categories.map(categories => <option key={categories.categories}>{categories.categories}</option>)}
+        </select>
+        <button onClick={quizStart}>start quiz</button>
+      </div>
     </div>
-   </div>
+
   )
 }
+
 
 export default App
--- Tietokantojen_Hyödyntäminen/Osio3/Tietovisa/index.js
+++ Tietokantojen_Hyödyntäminen/Osio3/Tietovisa/index.js
@@ -8,13 +8,34 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/categories', async (request,response) => {
-const { data, error } = await supabase.rpc('get_distinct_categories');
+const { data, error } = await supabase
+.rpc('get_distinct_categories');
 
 response.json(data)
 }) 
 
+app.get('/questions/:id', async (request, response) => {
+    const id = request.params.id
+    console.log(id)
+    const{data,error} = await supabase
+    .rpc("get_random_question", {category: id})
+    response.json(data)
+})
 
-app.get('/questions,')
+app.post('/records', async (request,response)=> {
+    const body = request.body
+
+    const {data,error} = await supabase
+    .from('records')
+    .insert({
+        username: body.username,
+        score: body.score
+    })
+    .select()
+    .single()
+
+    response.json(data)
+}) 
 
 
 const port = process.env.PORT || 3000
