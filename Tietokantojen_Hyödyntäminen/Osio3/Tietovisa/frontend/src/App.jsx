274648e84f25ab682c21db7825641bfad640913c
Leokayjones <leokayjones@gmail.com>
2025-11-14 09:53:04 +0200

    melkein tietovisa valmis puuttuu end screen ja highscore tallennus

--- Tietokantojen_Hyödyntäminen/Osio3/Tietovisa/frontend/src/App.jsx
+++ Tietokantojen_Hyödyntäminen/Osio3/Tietovisa/frontend/src/App.jsx
@@ -2,16 +2,23 @@ import React, { useState, useEffect } from "react"
 import axios from 'axios'
 import './app.css'
 
+
 function App() {
 
   const [categories, setCategories] = useState([])
+  const [selectedCategory, setSelectedCategories] = useState("")
+
   const [questions, setQuestions] = useState([])
   const [questionChoices, setQuestionChoices] = useState(null)
-  const [questionChoicesToShow, setQuestionChoicesToShow] = useState(null)
-  const [selectedCategory, setSelectedCategories] = useState("")
   const [currentQuestionIndex, setCurrentQuestionsIndex] = useState(0)
-  const [gameState, setGameState] = useState("start")
-  const [score, setScore] = useState(0)
+  const [questionChoicesToShow, setQuestionChoicesToShow] = useState(null)
+
+  const [records, setRecords] = useState([])
+  const [gameState, setGameState] = useState("highscore-end")
+  const [score, setScore] = useState(1)
+  const [highscore, setHighscore] = useState([])
+
+  const [newUsername, setNewUsername] = useState("")
 
 
   useEffect(() => {
@@ -43,18 +50,37 @@ function App() {
       })
   }
 
-  const checkCorrectAnswer = () => {
+  const saveHighScore = (event) => {
+    event.preventDefault()
+
+    const newUserInfo = {
+      username: newUsername,
+      score: score
+    }
+
+    axios.post('http://localhost:3000/records', newUserInfo)
+      .then((response) => {
+        setNewUsername("")
+        setRecords(records.concat(response.data))
+      })
+  }
+
+
+  const checkCorrectAnswer = (event) => {
+    var correctChoice = questionChoicesToShow.filter(correctAnswer => correctAnswer.right_choice)
+
+
+    if (Number(event.target.value) === correctChoice[0].id) {
+      setScore(score + 1)
+    }
+
     let nextIndex = currentQuestionIndex + 1
     setCurrentQuestionsIndex(nextIndex)
     setQuestionChoicesToShow(questionChoices.filter(choice => choice.question_id === questions[nextIndex].id))
-
-    if (questionChoicesToShow.right_choice === true) {
-      console.log("correct")
-      setScore(score + 1)
-    }
+    console.log(score)
   }
 
-  
+
   if (gameState === "start") {
     return (
       <div>
@@ -68,6 +94,14 @@ function App() {
             {categories.map(categories => <option key={categories.categories}>{categories.categories}</option>)}
           </select>
           <button onClick={quizStart}>start quiz</button>
+        </div>
+        <div>
+          <h2>Highscore</h2>
+          <div>
+            <ul>
+              <li></li>
+            </ul>
+          </div>
         </div>
       </div>
     )
@@ -83,13 +117,42 @@ function App() {
           <h1>{questions[currentQuestionIndex].questions}</h1>
         </div>
         <div className="QuizButtons">
-          <button onClick={checkCorrectAnswer} value={questionChoices.id}>{questionChoicesToShow[0].choice}</button>
-          <button onClick={checkCorrectAnswer} value={questionChoices.id}>{questionChoicesToShow[1].choice}</button>
-          <button onClick={checkCorrectAnswer} value={questionChoices.id}>{questionChoicesToShow[2].choice}</button>
+          <button onClick={checkCorrectAnswer} value={questionChoicesToShow[0].id}>{questionChoicesToShow[0].choice}</button>
+          <button onClick={checkCorrectAnswer} value={questionChoicesToShow[1].id}>{questionChoicesToShow[1].choice}</button>
+          <button onClick={checkCorrectAnswer} value={questionChoicesToShow[2].id}>{questionChoicesToShow[2].choice}</button>
         </div>
       </div>
     )
   }
+  if (gameState === "end") {
+    return (
+      <div>
+        <h1>Return to start</h1>
+        <div>
+          <button onClick={setGameState("start")}></button>
+        </div>
+      </div>
+    )
+
+
+  }
+  if (gameState === "highscore-end") {
+
+    return (
+      <div>
+        <div>
+          <h1>Save highscore</h1>
+          <form>
+            <input onChange={(e) => e.target.value} value={newUsername} placeholder="username"></input>
+            <button onClick={saveHighScore}>Save highscore</button>
+          </form>
+        </div>
+      </div>
+    )
+
+
+  }
 }
+
 
 export default App
